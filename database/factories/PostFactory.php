a34e7b011a82d63654bf2de619d5a5bf8f9cf10b
Mostafa Ahmed <modrsh2002@gmail.com>
2026-06-08 00:45:38 +0300

    add post seeder and factory to generate 500 post when seed

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // Generates a title
+            // Generates a title
+            'title' => fake()->sentence(3), 
+            
+            // Generates a description
+            'description' => fake()->paragraphs(3, true), 
+            
+            // Generates a random dates 
+            'created_at' => fake()->dateTimeBetween('-2 year', 'now'),
+            'updated_at' => function (array $attributes) {
+                return $attributes['created_at'];
+            },
+        ];
+    }
+}
