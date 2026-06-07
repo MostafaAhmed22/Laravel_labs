8d5eb1bdbdb4d8a51c8743b446ab47a4ce397ead
Mostafa Ahmed <modrsh2002@gmail.com>
2026-06-07 20:35:12 +0300

    create basic layout to use it in all coming views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,73 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+@section('title', 'Posts')
 
-<body class="bg-light">
-
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/home') }}">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="{{ url('/posts') }}">Posts</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container mt-5">
-        <div class="d-flex justify-content-between align-items-center mb-4">
-            <h2>All Posts</h2>
-        </div>
-
-        <div class="table-responsive bg-white p-3 rounded shadow-sm">
-            <table class="table table-striped align-middle">
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Description</th>
-                        <th scope="col" class="text-center">View</th>
-                        <th scope="col" class="text-center">Edit</th>
-                        <th scope="col" class="text-center">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($posts as $post)
-                    <tr>
-                        <td>{{ $post['title'] }}</td>
-                        <td>{{ $post['description'] }}</td>
-                        <td class="text-center">
-                            <a href="{{ url('/posts/show/'.$post['id']) }}" class="btn btn-sm text-white px-3" style="background-color: #5846f6; border: none;">Show</a>
-                        </td>
-                        <td class="text-center">
-                            <a href="#" class="btn btn-sm btn-warning text-white px-3" style="background-color: #ffca2c; border: none;">Edit</a>
-                        </td>
-                        <td class="text-center">
-                            <button class="btn btn-sm btn-danger px-3" style="background-color: #dc3545; border: none;">Delete</button>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+@section('content')
+<div class="container mt-5">
+    <h1 class="text-center text-primary mb-4">My Posts</h1>
+    <div class="table-responsive bg-white p-3 rounded shadow-sm">
+        <table class="table table-striped align-middle">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col" class="text-center">View</th>
+                    <th scope="col" class="text-center">Edit</th>
+                    <th scope="col" class="text-center">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($posts as $post)
+                <tr>
+                    <td>{{ $post['title'] }}</td>
+                    <td>{{ $post['description'] }}</td>
+                    <td class="text-center">
+                        <a href="{{ url('/posts/show/'.$post['id']) }}" class="btn btn-sm text-white px-3" style="background-color: #5846f6; border: none;">Show</a>
+                    </td>
+                    <td class="text-center">
+                        <a href="#" class="btn btn-sm btn-warning text-white px-3" style="background-color: #ffca2c; border: none;">Edit</a>
+                    </td>
+                    <td class="text-center">
+                        <button class="btn btn-sm btn-danger px-3" style="background-color: #dc3545; border: none;">Delete</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+</div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post Details</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+@section('title', 'Post Details')
 
-<body class="bg-light">
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/home') }}">ITIBlog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/posts') }}">Posts</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+@section('content')
     <!--  post details -->
     <div class="container mt-5">
         <div class="card shadow-sm col-md-8 mx-auto">
@@ -44,9 +18,4 @@
             </div>
         </div>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+@endsection
